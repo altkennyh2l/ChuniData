b81beefc958e38131af99b0315ebd86eea003d6c
Kenny Ha <77214322+altkennyh2l@users.noreply.github.com>
2022-09-30 19:04:16 +0900

    Beautiful code now, ty all around the internet

--- index.js
+++ index.js
@@ -10,6 +10,26 @@ let chartDB = await chartDB_json.json();
 let officialChartDB_json = await fetch(process.env.URL_OFFICIAL_JSON);
 let officialChartDB = await officialChartDB_json.json();
 
+const element = {
+  data: {
+    BAS: {
+      level: "",
+    },
+    ADV: {
+      level: "",
+    },
+    EXP: {
+      level: "",
+    },
+    MAS: {
+      level: "",
+    },
+    ULT: {
+      level: "",
+    },
+  },
+};
+
 await chartDB.forEach(async (element) => {
   if (element.meta.genre != "WORLD'S END") {
     let title = element.meta.title;
@@ -18,45 +38,11 @@ await chartDB.forEach(async (element) => {
     });
     element.meta.officialID = targetOfficialDB.id;
     element.meta.jacket = targetOfficialDB.image;
-    if (!element.data.hasOwnProperty("ULT")) {
-      element.data.BAS.level = element.data.BAS.level.toString();
-      element.data.ADV.level = element.data.ADV.level.toString();
-      element.data.EXP.level = element.data.EXP.level.toString();
-      element.data.MAS.level = element.data.MAS.level.toString();
-      if (element.data.BAS.level.endsWith(".5")) {
-        element.data.BAS.level = `${element.data.BAS.level.slice(0, -2)}+`;
-      }
-      if (element.data.ADV.level.endsWith(".5")) {
-        element.data.ADV.level = `${element.data.ADV.level.slice(0, -2)}+`;
-      }
-      if (element.data.EXP.level.endsWith(".5")) {
-        element.data.EXP.level = `${element.data.EXP.level.slice(0, -2)}+`;
-      }
-      if (element.data.MAS.level.endsWith(".5")) {
-        element.data.MAS.level = `${element.data.MAS.level.slice(0, -2)}+`;
-      }
-    } else {
-      element.data.BAS.level = element.data.BAS.level.toString();
-      element.data.ADV.level = element.data.ADV.level.toString();
-      element.data.EXP.level = element.data.EXP.level.toString();
-      element.data.MAS.level = element.data.MAS.level.toString();
-      element.data.ULT.level = element.data.ULT.level.toString();
-      if (element.data.BAS.level.endsWith(".5")) {
-        element.data.BAS.level = `${element.data.BAS.level.slice(0, -2)}+`;
-      }
-      if (element.data.ADV.level.endsWith(".5")) {
-        element.data.ADV.level = `${element.data.ADV.level.slice(0, -2)}+`;
-      }
-      if (element.data.EXP.level.endsWith(".5")) {
-        element.data.EXP.level = `${element.data.EXP.level.slice(0, -2)}+`;
-      }
-      if (element.data.MAS.level.endsWith(".5")) {
-        element.data.MAS.level = `${element.data.MAS.level.slice(0, -2)}+`;
-      }
-      if (element.data.ULT.level.endsWith(".5")) {
-        element.data.ULT.level = `${element.data.ULT.level.slice(0, -2)}+`;
-      }
+
+    for (const [key, val] of Object.entries(element.data)) {
+      element.data[key].level = val.level.toString().replace(/\.5$/u, "+");
     }
+
     await fetch(
       `${process.env.URL_OFFICIAL_IMG}${targetOfficialDB.image}`
     ).then((res) =>
